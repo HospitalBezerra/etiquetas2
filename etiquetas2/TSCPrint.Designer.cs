1d438e6aad718ead07ded3e80fbb0acd2e2a22e7
Luiz Henrique Pereira Niero <luiz_niero@hotmail.com>
2020-03-24 18:58:11 -0300

    Initial commit

---
+++ etiquetas2/TSCPrint.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace etiquetas2
+{
+	partial class TSCPrint
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.txtNomePaciente = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.txtNumeroProntuario = new System.Windows.Forms.TextBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.txtNomeMae = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.txtDataNascimento = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.button2 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(83, 288);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(210, 53);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "Imprimir";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.Location = new System.Drawing.Point(45, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(289, 20);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "Impressões de Pulseiras - Exemplo";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(12, 58);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(98, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Nome do Paciente:";
+			// 
+			// txtNomePaciente
+			// 
+			this.txtNomePaciente.Location = new System.Drawing.Point(131, 55);
+			this.txtNomePaciente.Name = "txtNomePaciente";
+			this.txtNomePaciente.Size = new System.Drawing.Size(240, 20);
+			this.txtNomePaciente.TabIndex = 1;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(12, 94);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(113, 13);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Número do Prontuário:";
+			// 
+			// txtNumeroProntuario
+			// 
+			this.txtNumeroProntuario.Location = new System.Drawing.Point(131, 91);
+			this.txtNumeroProntuario.Name = "txtNumeroProntuario";
+			this.txtNumeroProntuario.Size = new System.Drawing.Size(100, 20);
+			this.txtNumeroProntuario.TabIndex = 2;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 130);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(77, 13);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "Nome da Mãe:";
+			// 
+			// txtNomeMae
+			// 
+			this.txtNomeMae.Location = new System.Drawing.Point(131, 123);
+			this.txtNomeMae.Name = "txtNomeMae";
+			this.txtNomeMae.Size = new System.Drawing.Size(100, 20);
+			this.txtNomeMae.TabIndex = 3;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(12, 161);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(105, 13);
+			this.label5.TabIndex = 4;
+			this.label5.Text = "Data de nascimento:";
+			// 
+			// txtDataNascimento
+			// 
+			this.txtDataNascimento.Location = new System.Drawing.Point(131, 154);
+			this.txtDataNascimento.Name = "txtDataNascimento";
+			this.txtDataNascimento.Size = new System.Drawing.Size(100, 20);
+			this.txtDataNascimento.TabIndex = 4;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.BackColor = System.Drawing.Color.LightGray;
+			this.label6.Location = new System.Drawing.Point(90, 234);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(215, 13);
+			this.label6.TabIndex = 6;
+			this.label6.Text = "Obs: O nome da impressora deve ser \"TSC\"";
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(144, 198);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(75, 23);
+			this.button2.TabIndex = 7;
+			this.button2.Text = "Calibrar";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// TSCPrint
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(399, 353);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.txtDataNascimento);
+			this.Controls.Add(this.txtNomeMae);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.txtNumeroProntuario);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.txtNomePaciente);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button1);
+			this.Name = "TSCPrint";
+			this.Text = "Impressões de Pulseiras";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox txtNomePaciente;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox txtNumeroProntuario;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.TextBox txtNomeMae;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.TextBox txtDataNascimento;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button button2;
+	}
+}
+
---
+++ etiquetas2/TSCPrint.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace etiquetas2
+{
+	public partial class TSCPrint : Form
+		{
+			[DllImport("TSCLIB.dll", EntryPoint = "about")]
+			public static extern bool about();
+
+			[DllImport("TSCLIB.dll", EntryPoint = "openport")]
+			public static extern bool openport(string printer);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "barcode")]
+			public static extern int barcode(string x, string y, string type,
+			string height, string readable, string rotation,
+			string narrow, string wide, string code);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "clearbuffer")]
+			public static extern int clearbuffer();
+
+			[DllImport("TSCLIB.dll", EntryPoint = "closeport")]
+			public static extern int closeport();
+
+			[DllImport("TSCLIB.dll", EntryPoint = "downloadpcx")]
+			public static extern int downloadpcx(string filename, string image_name);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "formfeed")]
+			public static extern int formfeed();
+
+			[DllImport("TSCLIB.dll", EntryPoint = "nobackfeed")]
+			public static extern int nobackfeed();
+
+			[DllImport("TSCLIB.dll", EntryPoint = "printerfont")]
+			public static extern int printerfont(string x, string y, string fonttype,
+			string rotation, string xmul, string ymul,
+			string text);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "printlabel")]
+			public static extern int printlabel(string set, string copy);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "sendcommand")]
+			public static extern int sendcommand(string printercommand);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "setup")]
+			public static extern int setup(string width, string height,
+			string speed, string density,
+			string sensor, string vertical,
+			String offset);
+
+			[DllImport("TSCLIB.dll", EntryPoint = "windowsfont")]
+			public static extern int windowsfont(int x, int y, int fontheight,
+			int rotation, int fontstyle, int fontunderline,
+			string szFaceName, string content);
+			public TSCPrint()
+			{
+				InitializeComponent();
+				//about();
+			}
+
+			
+
+			private void button1_Click(object sender, EventArgs e)
+			{
+			string nomePaciente, nomeMae, numeroProntuario, dataNascimento;
+			nomePaciente = txtNomePaciente.Text;
+			nomeMae = txtNomeMae.Text;
+			numeroProntuario = txtNumeroProntuario.Text;
+			dataNascimento = txtDataNascimento.Text;
+
+			openport("TSC"); //A porta deve ser aberta no nome da impressora
+							 //	sendcommand("BLINE 4 mm, 115 mm");
+			//setup("25", "220", "10", "8", "1", "6", "10"); // Setup the media size and sensor type info
+														   /*SETUP DICIONARY: 
+															* setup(a, b, c, d, e, f, g)
+															* a: define a largura, em milímetros
+															* b: define a altura, em milímetros
+															* c: define a velocidade de impressão - min:1 max:12
+															* d: define a densidade do traço - min:0 max:15
+															* e: define o tipo do sensor usado - 0:GAP vertical, 1:sensor de marca negra
+															* f: define o tamanho do gap ou sensor negro, em milímetros
+															* g: define a distância do gap ou sensor negro.*/
+
+			clearbuffer(); // Clear image buffer
+						   //barcode("100", "100", "128", "100", "1", "0", "2", "2", "Barcode Test"); // Drawing barcode
+
+			/*PRINTERFONT DICIONARY:
+				 * printerfont(a, b, c, d, e, f, g)
+				 * a: define o ponto de início do texto no eixo X, em pontos. 1 ponto = 0.1mm
+				 * b: define o ponto de início do texto no eixo y, em pontos. ""
+				 * c: define tamanho da fonte: de 1 a 5
+				 * d: define o grau de rotação do texto
+				 * e: define a taxa de magnificação do texto ao longo do eixo x
+				 * f: idem, eixo y
+				 * g: define o conteúdo do texto a ser impresso
+				 */
+			//sendcommand("QRCODE 30,30,M,6,0,M2,\"BRADY123\"");
+			//sendcommand("BACKUP 80");
+			printerfont("160", "0", "2", "90", "1", "1", "NOVO"); // Drawing printer font
+			printerfont("120", "0", "2", "90", "1", "1", "TESTE");
+			printerfont("80", "0", "2", "90", "1", "1", "IMPRESS");
+			printerfont("40", "0", "2", "90", "1", "1", "TDP");
+
+			/*
+			printerfont("160", "0", "2", "90", "1", "1", "Nome do Paciente: " + nomePaciente); // Drawing printer font
+			printerfont("120", "0", "2", "90", "1", "1", "Prontuario: " + numeroProntuario); // Drawing printer font
+			printerfont("80",  "0", "2", "90", "1", "1", "Nome da Mae: " + nomeMae); // Drawing printer font
+			printerfont("40",  "0", "2", "90", "1", "1", "Data de Nasc.:" + dataNascimento); // Drawing printer font
+			*/
+
+
+
+			//windowsfont(100, 300, 24, 0, 0, 0, "ARIAL", "ti"); // Draw windows font
+			/*WINDOWFONT DICIONARY:
+			 * windowfont (a, b, c, d, e, f, g)
+			 * a: define o ponto de início do texto no eixo X, em pontos. 1 ponto = 0.1mm
+			 * b: define o ponto de início do texto no eixo y, em pontos. ""
+			 * c: define tamanho da fonte: de 1 a 5
+			 * d: define o grau de rotação do texto
+			 * e: define o estilo da fonte (0 - normal, 1 - italico, 2 - negrito, 3 - negrito e itálico)
+			 * f: define se a fonte é sublinhada (0 - não sublinhada, 1 - sublinhada)
+			 * g: define o nome da fonte (Arial, timew new roman, etc, de acordo com o sistema operacional)
+			 * h: define o conteúdo do texto a ser impresso
+			 */
+			//downloadpcx ("UL.PCX", "UL.PCX"); // Download PCX file into printer
+			//sendcommand ("PUTPCX 100,400, /" UL.PCX / ""); // Drawing PCX graphic
+			nobackfeed();
+			printlabel("1", "1"); // Print labels
+								  /*PRI-NTLABLE DICIONARY:
+								  * printlabel (a, b)
+								  * a: define o número de labels
+								  * b: define a quantidade de cópias
+								  */
+			//sendcommand("SET PEEL ON");
+			closeport(); // Close specified printer driver
+		
+
+			}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			openport("TSC");
+			sendcommand("SET GAP AUTO");
+			closeport();
+		}
+	}
+}
